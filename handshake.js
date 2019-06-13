aae6add0426576289ede5cb5815b900c27e39596
Michael Richards <mike22e@gmail.com>
2019-06-13 15:35:11 -0700

    Store auth tokens in local json file after handshake.

--- handshake.js
+++ handshake.js
@@ -2,11 +2,22 @@ const axios = require('axios')
 const open = require('open')
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 const { STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET, HANDSHAKE_PORT } = require('./config.json')
 
 const AUTH_URL = `http://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIENT_ID}&response_type=code&redirect_uri=http://localhost:${HANDSHAKE_PORT}/&approval_prompt=force&scope=read_all,activity:read_all,activity:write`
 
-;(() => {
+const store = auth => {
+  fs.writeFile("auth.json", JSON.stringify(auth), 'utf8', err => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log("Handshake successful. Your authentication details have been written to auth.json.")
+    }
+  })
+}
+
+(() => {
   const server = http.createServer(async (req, res) => {
     try {
       const { data } = await axios.post('https://www.strava.com/oauth/token', {
@@ -16,9 +27,11 @@ const AUTH_URL = `http://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIEN
         code: url.parse(req.url, true).query.code
       })
 
-      console.log('access token:', data.access_token)
-      console.log('refresh token:', data.refresh_token)
-      console.log('expires at:', new Date(data.expires_at * 1000))
+      store({
+        access_token: data.access_token,
+        refresh_token: data.refresh_token,
+        expires_at: data.expires_at
+      })
 
       res.end('handshake successful')
     } catch(error) {
